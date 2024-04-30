5b6bd1318c4316d8ff9782809c22518f5be2d9df
akuowen <128798896+akuowen@users.noreply.github.com>
2024-04-30 23:20:07 +0800

    Create matrix.rs

---
+++ src/matrix.rs
@@ -0,0 +1,103 @@
+use anyhow::Result;
+use std::fmt;
+use std::ops::{AddAssign, Mul};
+pub struct Matrix<T> {
+    data: Vec<T>,
+    rows: usize,
+    cols: usize,
+}
+
+impl<T: fmt::Debug> Matrix<T> {
+    pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
+        Self {
+            data: data.into(),
+            rows: row,
+            cols: col,
+        }
+    }
+}
+
+impl<T> fmt::Display for Matrix<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{")?;
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                write!(f, "{}", self.data[i * self.cols + j])?;
+                if j != self.cols - 1 {
+                    write!(f, " ")?;
+                }
+            }
+
+            if i != self.rows - 1 {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "}}")?;
+        Ok(())
+    }
+}
+
+impl<T> fmt::Debug for Matrix<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Matrix(row={}, col={}, {})", self.rows, self.cols, self)
+    }
+}
+
+pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
+where
+    T: std::ops::Add<Output = T> + Copy + Default + std::fmt::Debug + Mul<Output = T> + AddAssign,
+{
+    if a.cols != b.rows {
+        return Err(anyhow::anyhow!(
+            "Matrix A cols must be equal to Matrix B rows"
+        ));
+    }
+    let mut data = vec![T::default(); a.rows * b.cols];
+    for i in 0..a.rows {
+        for j in 0..b.cols {
+            for k in 0..a.cols {
+                data[i * b.cols + j] += a.data[i * a.cols + k] * b.data[k * b.cols + j];
+            }
+        }
+    }
+
+    Ok(Matrix {
+        data,
+        rows: a.rows,
+        cols: b.cols,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matrix_multiply() -> Result<()> {
+        let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
+        let c = multiply(&a, &b)?;
+        assert_eq!(c.cols, 2);
+        assert_eq!(c.rows, 2);
+        assert_eq!(c.data, vec![22, 28, 49, 64]);
+        assert_eq!(format!("{:?}", c), "Matrix(row=2, col=2, {22 28, 49 64})");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_matrix_display() -> Result<()> {
+        let a = Matrix::new([1, 2, 3, 4], 2, 2);
+        let b = Matrix::new([1, 2, 3, 4], 2, 2);
+        let c = multiply(&a, &b)?;
+        assert_eq!(c.data, vec![7, 10, 15, 22]);
+        assert_eq!(format!("{}", c), "{7 10, 15 22}");
+        Ok(())
+    }
+}
